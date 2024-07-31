1ced4d2f42fbfb4afa4b341d56019196a3ed0a93
Rahmakammoun <rahmakammoun01@gmail.com>
2024-07-31 09:32:29 +0100

    third commit

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,36 +3,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/user';
 
-export const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
 
-  try {
-    const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
-
-    
-    if (user.role !== 'admin') {
-      return res.status(403).json({ message: 'You are not authorized to login' });
-    }
-
-    const token = jwt.sign(
-      { id: user.id, role: user.role },
-      process.env.JWT_SECRET!,
-      { expiresIn: '1h' }
-    );
-
-    return res.json({ message: 'Admin login successfully', token });
-  } catch (err) {
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-};
 
 
 export const createFournisseur = async (req: Request, res: Response) => {
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { login, createFournisseur, loginFournisseur } from '../controllers/userController';
+import {  createFournisseur, loginFournisseur } from '../controllers/userController';
 
 const router = Router();
 
-router.post('/login', login);
+
 router.post('/createfournisseur', createFournisseur);
 router.post('/login-fournisseur', loginFournisseur); 
 
